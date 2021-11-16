86a656fb7a76bd76c46a8d83690bea45788b3d68
ingeni <ingeni_killer@hotmail.com>
2021-11-16 08:31:43 +0100

    première version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { ListecomptesComponent } from './listecomptes/listecomptes.component';
+import { ListeoperationsComponent } from './listeoperations/listeoperations.component';
+import { PageoperationComponent } from './pageoperation/pageoperation.component';
+import { ListefluxComponent } from './listeflux/listeflux.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'listecomptes',
+    component: ListecomptesComponent
+  },
+  {
+    path: 'listeoperations',
+    component: PageoperationComponent
+  },
+  {
+    path: 'listeflux',
+    component: ListefluxComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule  } from '@angular/common/http';
+
+import { LoginComponent } from './login/login.component';
+import { ListecomptesComponent } from './listecomptes/listecomptes.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListeoperationsComponent } from './listeoperations/listeoperations.component';
+import { PageoperationComponent } from './pageoperation/pageoperation.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { ListefluxComponent } from './listeflux/listeflux.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    LoginComponent,
+    ListecomptesComponent,
+    MenuComponent,
+    ListeoperationsComponent,
+    PageoperationComponent,
+    PaginationComponent,
+    ListefluxComponent
+    
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
